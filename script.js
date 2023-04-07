92ed3997db046ea5aeff5d4afeb579eb2b8db6d5
AnnaAkker <AnnaBorSi@yandex.ru>
2023-04-08 02:38:23 +0300

    Third

---
+++ script.js
@@ -0,0 +1,57 @@
+const main = document.querySelector('.page');
+
+const profile = main.querySelector('.profile');
+const nameProfile = profile.querySelector('.profile__name');
+const subtitleProfile = profile.querySelector('.profile__subtitle');
+
+const elements = document.querySelector('.elements');
+const likeButton = elements.querySelector('.elements__like-button');
+
+const popup = main.querySelector('.popup');
+const editProfile = document.querySelector('.popup-profile');
+const editProfileButton = document.querySelector('.profile__button-edit');
+const closeProfileButton = document.querySelector('.popup__button-close');
+const popupform = document.querySelector('.popup__form');
+const nameInput = document.querySelector('.popup__input_type_name');
+const subtitleInput = document.querySelector('.popup__input_type_subtitle');
+
+// Попап редактировать профиль //
+
+function openEditePopup(popup) {
+    popup.classList.add('popup_opened');
+}
+  
+function closePopup(popup) {
+    popup.classList.remove('popup_opened');
+}
+  
+function handleFormSubmit(evt) {
+    evt.preventDefault();
+    nameProfile.textContent = nameInput.value;
+    subtitleProfile.textContent = subtitleInput.value;
+    closePopup(editProfile);
+}
+  
+function addEventListeners() {
+    popupform.addEventListener('submit', handleFormSubmit);
+  
+    editProfileButton.addEventListener('click', function() {
+      nameInput.value = nameProfile.textContent;
+      subtitleInput.value = subtitleProfile.textContent;
+      openEditePopup(editProfile);
+    });
+  
+    closeProfileButton.addEventListener('click', function() {
+      closePopup(editProfile);
+    });
+}
+  
+addEventListeners();
+
+// Кнопка нажатия на сердечко//
+
+function toggleLikeButton() {
+    likeButton.classList.toggle('elements__like-button_active');
+};
+  
+likeButton.addEventListener('click', toggleLikeButton);
