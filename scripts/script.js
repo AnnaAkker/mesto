309532ecca568727f988b33e3d07a354aa7479e8
AnnaAkker <AnnaBorSi@yandex.ru>
2023-04-09 18:31:01 +0300

    1- review

--- scripts/script.js
+++ scripts/script.js
@@ -4,11 +4,7 @@ const profile = main.querySelector('.profile');
 const nameProfile = profile.querySelector('.profile__name');
 const subtitleProfile = profile.querySelector('.profile__subtitle');
 
-const elements = document.querySelector('.elements');
-const likeButtons = elements.querySelectorAll('.elements__like-button');
-
 const popup = main.querySelector('.popup');
-const editProfile = document.querySelector('.popup-profile');
 const editProfileButton = document.querySelector('.profile__button-edit');
 const closeProfileButton = document.querySelector('.popup__button-close');
 const popupform = document.querySelector('.popup__form');
@@ -18,42 +14,29 @@ const subtitleInput = document.querySelector('.popup__input_type_subtitle');
 // Попап редактировать профиль //
 
 function openEditePopup(popup) {
-    popup.classList.add('popup__opened');
+    popup.classList.add('popup_opened');
 }
   
 function closePopup(popup) {
-    popup.classList.remove('popup__opened');
+    popup.classList.remove('popup_opened');
 }
   
 function handleFormSubmit(evt) {
     evt.preventDefault();
     nameProfile.textContent = nameInput.value;
     subtitleProfile.textContent = subtitleInput.value;
-    closePopup(editProfile);
+    closePopup(popup);
 }
   
-function addEventListeners() {
-    popupform.addEventListener('submit', handleFormSubmit);
-  
-    editProfileButton.addEventListener('click', function() {
-      nameInput.value = nameProfile.textContent;
-      subtitleInput.value = subtitleProfile.textContent;
-      openEditePopup(editProfile);
-    });
-  
-    closeProfileButton.addEventListener('click', function() {
-      closePopup(editProfile);
-    });
-}
-  
-addEventListeners();
 
-// Кнопка нажатия на сердечко//
+popupform.addEventListener('submit', handleFormSubmit);
 
-function toggleLikeButton(event) {
-    event.target.classList.toggle('elements__like-button_active');
-}
-  
-likeButtons.forEach(function(button) {
-    button.addEventListener('click', toggleLikeButton);
+editProfileButton.addEventListener('click', function() {
+    nameInput.value = nameProfile.textContent;
+    subtitleInput.value = subtitleProfile.textContent;
+    openEditePopup(popup);
+});
+
+closeProfileButton.addEventListener('click', function() {
+    closePopup(popup);
 });
