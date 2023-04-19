61d99eb6da93ca1f8c8c6e5a6e2db671416ae742
AnnaAkker <AnnaBorSi@yandex.ru>
2023-04-20 01:44:22 +0300

    5 ПР

--- scripts/index.js
+++ scripts/index.js
@@ -5,11 +5,19 @@ const nameProfile = profile.querySelector('.profile__name');
 const subtitleProfile = profile.querySelector('.profile__subtitle');
 
 const popup = main.querySelector('.popup');
+
+const EditePopup = document.querySelector('.EditePopup');
 const editProfileButton = document.querySelector('.profile__button-edit');
 const closeProfileButton = document.querySelector('.popup__button-close');
 const popupform = document.querySelector('.popup__form');
+
 const nameInput = document.querySelector('.popup__input_type_name');
 const subtitleInput = document.querySelector('.popup__input_type_subtitle');
+
+const CardPopup = document.querySelector('.CardPopup');
+const titleCardPopup = document.querySelector('.popup__input_type_description_title');
+const imageCardPopup = document.querySelector('.popup__input_type_image');
+
 
 // Попап редактировать профиль //
 
@@ -40,3 +48,5 @@ editProfileButton.addEventListener('click', function() {
 closeProfileButton.addEventListener('click', function() {
     closePopup(popup);
 });
+
+
