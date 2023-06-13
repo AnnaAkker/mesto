d871d9e1b4999c008000c3cbe565239e9c2a4a14
AnnaAkker <AnnaBorSi@yandex.ru>
2023-06-13 20:29:22 +0300

    7 ПР

--- scripts/validate.js
+++ scripts/validate.js
@@ -1,99 +1,3 @@
-// const validConfig = {
-//     formSelector: '.popup__form',
-//     inputSelector: '.popup__input',
-//     submitButtonSelector: '.popup__submit-button',
-//     disabledButtonClass: 'popup__submit-button_disabled',
-//     inputErrorClass: 'popup__input_invalid',
-//     errorClass: 'popup__input-error'
-// };
-
-// // Валидность инпутов //
-
-// const hasInvalidInput = (inputList) => {
-//   return inputList.some((inputObject) => {
-//     return !inputObject.validity.valid;
-//   });
-// };
-
-// // Функция состояния кнопки //
-
-// function toggleButtonState(validConfig, inputList, buttonElement) {
-//   if (hasInvalidInput(inputList)) {
-//     buttonElement.classList.add(validConfig.disabledButtonClass);
-//     buttonElement.setAttribute('disabled', true);
-//   } else {
-//     buttonElement.removeAttribute('disabled');
-//     buttonElement.classList.remove(validConfig.disabledButtonClass);
-//   };
-// };
-
-// // Показ ошибки //
-
-// const showInputError = (validConfig, formObject, inputObject, errorMessage) => {
-//   const errorElement = formObject.querySelector(`.${inputObject.id}-error`);
-//   inputObject.classList.add(validConfig.errorClass);
-//   errorElement.textContent = errorMessage;
-//   inputObject.classList.add(validConfig.inputErrorClass);
-// };
-
-// // Скрытие ошибки // 
-
-// const hideInputError = (validConfig, formObject, inputObject) => {
-//   const errorElement = formObject.querySelector(`.${inputObject.id}-error`);
-//   inputObject.classList.remove(validConfig.errorClass);
-//   errorElement.textContent = '';
-//   inputObject.classList.remove(validConfig.inputErrorClass);
-// };
-
-// // Валидность //
-
-// const isValid = (validConfig, formObject, inputObject, inputList, buttonElement) => {
-//   if (!inputObject.validity.valid) {
-//     showInputError(validConfig, formObject, inputObject, inputObject.validationMessage);
-//   } else { 
-//     hideInputError(validConfig, formObject, inputObject);
-//   };
-
-//   inputList.forEach((inputObject) => {
-//     inputObject.addEventListener('input', function () {
-//       isValid(validConfig, formObject, inputObject, inputList, buttonElement);
-//       toggleButtonState(validConfig, inputList, buttonElement);
-//     });
-//   });
-// };
-
-// // Функция для добавления обработчиков событий на все поля формы //
-
-// const setEventListeners = (validConfig, formObject) => {
-//   const inputList = Array.from(formObject.querySelectorAll(validConfig.inputSelector));
-//   const buttonElement = formObject.querySelector(validConfig.submitButtonSelector);
-
-//   formObject.addEventListener('submit', (evt) => {
-//       evt.preventDefault();
-//       formObject.reset();
-//       toggleButtonState(validConfig, inputList, buttonElement);
-//   });
-
-//   toggleButtonState(validConfig, inputList, buttonElement);
-
-//   inputList.forEach((inputObject) => {
-//     inputObject.addEventListener('input', function () {
-//       isValid(validConfig, formObject, inputObject, inputList, buttonElement);
-//       toggleButtonState(validConfig, inputList, buttonElement);
-//     });
-//   });
-// };
-
-// const enableValidation = (validConfig) => {
-//   const formsList = Array.from(document.querySelectorAll(validConfig.formSelector));
-  
-//   formsList.forEach((formObject) => {
-//     setEventListeners(validConfig, formObject);
-//   });
-// };
-
-// enableValidation(validConfig);
-
 class FormValidator {
   constructor(validConfig) {
     this._formSelector = validConfig.formSelector;
@@ -104,12 +8,16 @@ class FormValidator {
     this._errorClass = validConfig.errorClass;
   };
 
+  // Показ ошибки //
+
   _showInputError(formObject, inputObject, errorMessage) {
     const errorElement = formObject.querySelector(`.${inputObject.id}-error`);
     inputObject.classList.add(this._errorClass);
     errorElement.textContent = errorMessage;
     inputObject.classList.add(this._inputErrorClass);
   };
+  
+  // Скрытие ошибки // 
 
   _hideInputError(formObject, inputObject) {
     const errorElement = formObject.querySelector(`.${inputObject.id}-error`);
@@ -117,6 +25,8 @@ class FormValidator {
     errorElement.textContent = '';
     inputObject.classList.remove(this._inputErrorClass);
   };
+
+  // Валидность инпутов //
 
   _checkInputValidity(formObject, inputObject) {
     if (!inputObject.validity.valid) {
